102b9b67ac0dfda2d469871c4c2c69b0819c9805
root <root@1cf8b3e25644>
2022-06-15 23:49:36 +0000

    Finished Awesome and Scary Maze

--- src/_03_print_and_popups/_5_awesome_or_not/AwesomeOrNot.java
+++ src/_03_print_and_popups/_5_awesome_or_not/AwesomeOrNot.java
@@ -2,26 +2,45 @@ package _03_print_and_popups._5_awesome_or_not;
 
 import java.util.Random;
 
+import javax.swing.JOptionPane;
+
 public class AwesomeOrNot {
 
 	// 1. Make a main method that includes everything below
-		public static void main(String[] args) {
+	public static void main(String[] args) {
 			
 		
 	
 		Random ran = new Random();
 		int number = ran.nextInt(4); 
 		System.out.println(number); 
+		String question = JOptionPane.showInputDialog("What do you think is awesome?");
+		if(number==0) {
+			JOptionPane.showMessageDialog(null,"What you entered is awesome!"); 
 		
+		}
+		else if(number==1) {
+			JOptionPane.showMessageDialog(null,"What you entered is okay");
+		}
+		
+		else if(number==2) {
+			JOptionPane.showMessageDialog(null, "What you entered is boring");
+		}
+		else if(number==3) {
+			JOptionPane.showMessageDialog(null, "What you entered is not true");
+		}
 		//This will b used below to make a random number. 
 		
+		
 		// 2. Make a variable that will hold a random whole number
+		
 	
 		// 3. Set your variable equal to a positive number less than 4 using     ran.nextInt(4); 
 	
 		// 3. Print your variable to the console
 	
 		// 4. Get the user to enter something that they think is awesome
+		
 	
 		// 5. If your variable is  0
 	
